4e450a6ab933dc4a709dd7662d3f389f9fef516b
Taras Samoilov <tsasojlov@icloud.com>
2025-01-31 12:38:45 +0200

    added some animations to liven up the page

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var swiper = new Swiper('.mySwiper', {
 	slidesPerView: 'auto',
-	// centeredSlides: true,
 	spaceBetween: 30,
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
 		dynamicBullets: true,
 	},
+})
+document.addEventListener('DOMContentLoaded', () => {
+	const title = document.querySelector('.main__title')
+	const text = title.textContent
+	title.innerHTML = ''
+
+	text.split('').forEach((char, index) => {
+		const span = document.createElement('span')
+		if (char === ' ') {
+			span.innerHTML = '&nbsp;'
+		} else {
+			span.textContent = char
+		}
+		span.style.animationDelay = `${index * 0.05}s`
+		title.appendChild(span)
+	})
 })
